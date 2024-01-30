dbe527c33bad24fba444c7c418b3ea155a4578ac
root <root@VIetLaptop>
2024-01-30 15:48:45 -0600

    added name and ID

--- Homework_1/src/Homework_1.java
+++ Homework_1/src/Homework_1.java
@@ -2,6 +2,7 @@ public class Homework_1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Chetan Vanteddu - GH_ID: 87617260");
+		System.out.println("Viet Nguyen - GH_ID: 157855299");
 	}
 
 }
