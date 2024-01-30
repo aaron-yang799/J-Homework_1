426ad965b015539cf9cc765112a7277cfaebc74f
chesain <cvanteddu1@gmail.com>
2024-01-30 13:36:06 -0600

    Added GH ID

--- Homework_1/src/Homework_1.java
+++ Homework_1/src/Homework_1.java
@@ -1,7 +1,7 @@
 public class Homework_1 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Chetan Vanteddu");
+		
+		System.out.println("Chetan Vanteddu - GH_ID: 87617260");
 	}
 
 }
