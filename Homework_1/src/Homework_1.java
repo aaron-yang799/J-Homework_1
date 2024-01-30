dce6a556f4c66e8fb7407eb58a97e27122be52f2
aaron-yang799 <aaron.yang799@gmail.com>
2024-01-30 16:08:46 -0600

    Added my line of code

--- Homework_1/src/Homework_1.java
+++ Homework_1/src/Homework_1.java
@@ -4,6 +4,7 @@ public class Homework_1 {
 		System.out.println("Chetan Vanteddu - GH_ID: 87617260");
 		System.out.println("Viet Nguyen - GH_ID: 157855299");
 		System.out.println("Zach Snyder - GH_ID: 118587176");
+		System.out.println("Aaron Yang - GH_ID: 146474655");
 	}
 
 }
