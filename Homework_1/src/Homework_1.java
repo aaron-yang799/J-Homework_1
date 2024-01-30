6c39009149b62ce3567dc534b14745c791446466
zachrsnyder <zachrsnyder2004@gmail.com>
2024-01-30 15:54:39 -0600

    Added my line

--- Homework_1/src/Homework_1.java
+++ Homework_1/src/Homework_1.java
@@ -3,6 +3,7 @@ public class Homework_1 {
 		
 		System.out.println("Chetan Vanteddu - GH_ID: 87617260");
 		System.out.println("Viet Nguyen - GH_ID: 157855299");
+		System.out.println("Zach Snyder - GH_ID: 118587176");
 	}
 
 }
