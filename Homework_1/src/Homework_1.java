997ac0afb1af8b4c368df3536b9ce71376429f6b
veetvqn <vietqnagooyan@gmail.com>
2024-01-30 21:44:13 -0600

    Fixed git commit

--- Homework_1/src/Homework_1.java
+++ Homework_1/src/Homework_1.java
@@ -2,11 +2,8 @@ public class Homework_1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Chetan Vanteddu - GH_ID: 87617260");
-		System.out.println("Viet Nguyen - GH_ID: 157855299");
 		System.out.println("Zach Snyder - GH_ID: 118587176");
 		System.out.println("Aaron Yang - GH_ID: 146474655");
-		
-		
+		System.out.println("Viet Nguyen - GH_ID: 157855299");
 	}
-
 }
